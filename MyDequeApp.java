59a7c284bd7d012291d25d63bb19b7d6855c9407
ElBmann <brianrecuero@gmail.com>
2016-03-19 09:54:08 -0400

    Update MyDequeApp.java

--- MyDequeApp.java
+++ MyDequeApp.java
@@ -1,36 +1,40 @@
+package dataStructures;
+
+import dataStructures.MyDeque;
+
 /**
  * Created by Brecuero on 3/19/2016.
  */
 public class MyDequeApp {
         public void main(String[] args)
         {
-            MyDeque thedeQueue = new MyDeque(5); //........................................queue holds 5 items
+            MyDeque theDeQueue = new MyDeque(5); //........................................queue holds 5 items
 
-            thedeQueue.insertFront(10); // insert 4 items
+            theDeQueue.insertFront(10); // insert 4 items
 
-            thedeQueue.insertFront(20);
+            theDeQueue.insertFront(20);
 
-            thedeQueue.insertFront(30);
+            theDeQueue.insertFront(30);
 
-            thedeQueue.insertFront(40);
+            theDeQueue.insertFront(40);
 
-            thedeQueue.removeFront(); // remove 3 items
+            theDeQueue.removeFront(); // remove 3 items
 
-            thedeQueue.removeFront(); // (10, 20, 30)
+            theDeQueue.removeFront(); // (10, 20, 30)
 
-            thedeQueue.removeFront();
+            theDeQueue.removeFront();
 
-            thedeQueue.insertFront(50); // insert 4 more items
+            theDeQueue.insertFront(50); // insert 4 more items
 
-            thedeQueue.insertFront(60); // (wraps around)
+            theDeQueue.insertFront(60); // (wraps around)
 
-            thedeQueue.insertFront(70);
+            theDeQueue.insertFront(70);
 
-            thedeQueue.insertFront(80);
+            theDeQueue.insertFront(80);
 
-            while( !thedeQueue.isEmpty() ) // remove and display
+            while( !theDeQueue.isEmpty() ) // remove and display
             { // all items
-                long n = thedeQueue.removeFront(); // (40, 50, 60, 70, 80)
+                long n = theDeQueue.removeFront(); // (40, 50, 60, 70, 80)
 
                 System.out.print(n);
                 System.out.print(" ");
