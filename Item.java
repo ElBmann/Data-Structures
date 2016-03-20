300b84b2b6f9d01c18bcae9dc984cdea3ac64a2b
ElBmann <brianrecuero@gmail.com>
2016-03-19 21:51:35 -0400

    Added files via upload

---
+++ Item.java
@@ -0,0 +1,38 @@
+package dataStructures;
+
+/**
+ * Created by Brecuero on 3/19/2016.
+ */
+public class Item
+{
+    private String item;
+    private double itemPrice;
+    public Item()
+    {
+        item= "Computer";
+        itemPrice=1500.00;
+    }
+    Item(String newItem, double newItemPrice)
+    {
+        item=newItem;
+        itemPrice=newItemPrice;
+    }
+    public String getItem()
+    {
+    return item;
+    }
+     public void setItem(String newItem)
+    {
+        item = newItem;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(double newItemPrice)
+    {
+        itemPrice=newItemPrice;
+    }
+}
+
