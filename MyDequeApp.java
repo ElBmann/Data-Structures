f53da83d47b60d5761bce73631d956d9c9355635
ElBmann <brianrecuero@gmail.com>
2016-03-22 08:22:34 -0400

    Update MyDequeApp.java

--- MyDequeApp.java
+++ MyDequeApp.java
@@ -9,130 +9,110 @@ public class MyDequeApp
 
         public static void main(String[] args)
         {
-            Scanner input = new Scanner(System.in);//....................................................Allows user to input
+            Scanner input = new Scanner(System.in);//..................................................................Allows user to input
             int choice=0;
             int change;
-            System.out.println("Dequeue Manipulation Enter Size of Deque \n");
-            change = input.nextInt();//.................................................................Allows user to change the size of the Dequeue
-            MyDeque theDeQueue = new MyDeque(change);//.................................................Queue holds 5 items
+            System.out.println("Dequeue Manipulation Enter Size of Deque: ");
+            change = input.nextInt();//................................................................................Allows user to change the size of the Dequeue
+            MyDeque theDeQueue = new MyDeque(change);//................................................................Dequeue
             while(choice!=8)
             {
 
-
-                System.out.println("Please Select a number in the menu: \n" + " 1: For insert Front \n 2: For insert rear \n 3: For remove Front \n 4: For Remove Rear  " +
+                System.out.println("Please Select a number in the menu: \n" + " 1: For insert Front \n " +
+                        "2: For insert rear \n 3: For remove Front \n 4: For Remove Rear  " +
                         "\n 5: For Peek Front \n 6: For Peek rear \n 7: For Display queue \n 8: For Quit  ");
-                         choice = input.nextInt();//................................................User Options
+                         choice = input.nextInt();//.................................................................. User Options
 
                 switch (choice)
-                {//..................................................................................Switch Case Menu
+                {//................................................................................................... Switch Case Menu
                     case 1:
-
-                        try//........................................................................This is what we want anything else the catch will block it and display a message
-                         {
-                            String sku;//............................................................Variable for Item Number
-                            double Price;//..........................................................Variable for Price
+                            String sku;//............................................................................. Variable for Item Number
+                            double itemPrice;//........................................................................Variable for Price
                                if(theDeQueue.isFull())
                                    System.out.println("Is Full");
                             else
+                                     try//.............................................................................This is what we want anything else the catch will block it and display a message
+                                     {
+                                   System.out.println("Please enter the Sku number you wish to insert.");//............Prompt for the item number
+                                   input.nextLine();//.................................................................clear carriage return
+                                   sku = input.nextLine();
 
-                                   System.out.println("Please enter the item number you wish to insert."); // prompt for the item number
-                            input.nextLine(); // clear carriage return
-                            sku = input.nextLine();
+                                   System.out.println("Please enter the item price.");//...............................Prompt for the item price
+                                   itemPrice = input.nextDouble();
+                                   Item newItem = new Item(sku,itemPrice);//...........................................Create an item with the given number and price
 
-                            System.out.println("Please enter the item price."); // prompt for the item price
-                            Price = input.nextDouble();
-
-                            Item newItem = new Item(); // create an item with the given number and price
-
-                            theDeQueue.insertFront(newItem); // insert the item
+                                   theDeQueue.insertFront(sku,itemPrice);//............................................Insert the item
                          }
                            catch(Exception d)
-                              {//...................................................................makes sure their is no string input by user
+                              {//......................................................................................Makes sure their is no string input by user
                                  System.out.println("no Letters Please: \n");
-                                input.next();
+                                input.nextLine();
 
                             }
                         break;
                     case 2:
-                       try//........................................................................This is what we want anything else the catch will block it and display a message
-                       {
-                       String sku;//............................................................Variable for Item Number
-                       double Price;//..........................................................Variable for Price
-                            if(theDeQueue.isFull())
-                                System.out.println("Is Full");
-                            else
 
-                                System.out.println("Please enter the item number you wish to insert."); // prompt for the item number
-                            input.nextLine(); // clear carriage return
-                            sku= input.nextLine();
+                           if(theDeQueue.isFull())
+                               System.out.println("Is Full");
+                           else
+                               try//...................................................................................This is what we want anything else the catch will block it and display a message
+                               {
+                                   System.out.println("Please enter the Sku number you wish to insert.");//............Prompt for the item number
+                                   input.nextLine();//.................................................................clear carriage return
+                                   sku = input.nextLine();
 
-                            System.out.println("Please enter the item price."); // prompt for the item price
-                            Price= input.nextDouble();
+                                   System.out.println("Please enter the item price.");//...............................Prompt for the item price
+                                   itemPrice = input.nextDouble();
+                                   Item newItem = new Item(sku,itemPrice);//...........................................Create an item with the given number and price
 
-                            Item newItem2 = new Item(); // create an item with the given number and price
+                                   theDeQueue.insertRear(sku,itemPrice);//.............................................Insert the item
+                               }
+                               catch(Exception d) {//..................................................................Makes sure their is no string input by user
+                                   System.out.println("no Letters Please: \n");
+                                   input.nextLine();
+                               }
 
-                            theDeQueue.insertRear(newItem2); // insert the item
-                          }
-                        catch(Exception d)
-                        {//makes sure their is no string input by user
-                            System.out.println("no Letters Please: \n");
-                            input.next();
 
-                        }
                         break;
                     case 3:
-                        if(theDeQueue.isEmpty()) // if the deque is empty, cannot remove
-                            System.out.println("Sorry, the deque is empty. Cannot remove.");
+                        if(theDeQueue.isEmpty())//.....................................................................If the deque is empty, cannot remove
+                            System.out.println("There is Nothing HERE SON!!!!");
 
                         else
-                            System.out.println("You removed: " + theDeQueue.removeFront().getSku());
+                            System.out.println("You removed: " + theDeQueue.removeFront().getSku());//.................All Sku Numbers are Removed From Front
 
                         break;
                     case 4:
-                        if(theDeQueue.isEmpty()) // if the deque is empty, cannot remove
+                        if(theDeQueue.isEmpty())//.....................................................................If the deque is empty, cannot remove
                             System.out.println("Sorry, the deque is empty. Cannot remove.");
                         else
                             System.out.println("You removed: " + theDeQueue.removeRear().getSku());
 
                         break;
                     case 5:
-                        if(theDeQueue.isEmpty()) // if the deque is empty, cannot peek
+                        if(theDeQueue.isEmpty())//.....................................................................If the deque is empty, cannot peek
                             System.out.println("The deque is empty. There is nothing to peek.");
                         else
-                            System.out.println("The front is: " + theDeQueue.peekFront().getSku() + "@ $" + theDeQueue.peekFront().getItemPrice());
+                            System.out.println("The front is: " + theDeQueue.peekFront().getSku() + "@ $" + theDeQueue.peekFront().getItemPrice());//.......??
 
                         break;
                     case 6:
-                        if(theDeQueue.isEmpty()) // if the deque is empty, cannot peek
+                        if(theDeQueue.isEmpty())//.....................................................................If the deque is empty, cannot peek
                             System.out.println("The deque is empty. There is nothing to peek.");
                         else
-                            System.out.println("The rear is: " + theDeQueue.peekRear().getSku() + "@ $" + theDeQueue.peekRear().getItemPrice());
+                            System.out.println("The rear is: " + theDeQueue.peekRear().getSku() + "@ $" + theDeQueue.peekRear().getItemPrice());//.............??
 
                         break;
                     case 7:
-                        System.out.println("The state is: " + theDeQueue.toString());
-
+                        System.out.println("The state is: " + theDeQueue.toString());//................................If Display Queue
 
                         break;
                     case 8:
                         break;
                     default:
-                        System.out.println("You Didn't Enter any of the Options");//..............If User Chooses a number out of bounds
+                        System.out.println("You Didn't Enter any of the Options");//...................................If User Chooses a number out of bounds
                 }
             }
-           /* while( !theDeQueue.isEmpty() ) // remove and display
-            { // all items
-                Item n = theDeQueue.removeFront(); // (40, 50, 60, 70, 80)
-
-                System.out.print(n);
-                System.out.print(" ");
-            }
-
-            System.out.println("");
-            */
 
         }
     }
-
-
-
