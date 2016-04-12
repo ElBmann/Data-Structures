cb35132af5c7c76d0c4a5c1da32852376695620b
ElBmann <brianrecuero@gmail.com>
2016-04-11 21:56:24 -0400

    Update MyDeque.java

--- MyDeque.java
+++ MyDeque.java
@@ -1,14 +1,20 @@
 
 /**
- * Created by Brecuero on 3/19/2016.
+ * Created by Brian Recuero on 3/19/2016.
+ * My Deque Class is written In Java
+ * This class has the Unique set up that allows a user to
+ * insert front & rear also Remove front & rear
+ * another feature is peek front and peek rear which allows the user
+ * to see the front and or rear elements without touching them.
  */
 package dataStructures;
-public class MyDeque {
-    private int maxSize;
-    private Item[] dequeArray;
-    private int front;
-    private int rear;
-    private int nItems=0;
+public class MyDeque
+{
+    private int maxSize;//.....................................................................Initialize Variables
+    private Item[] dequeArray;//...............................................................Initialize Variables
+    private int front;//.......................................................................Initialize Variables
+    private int rear;//........................................................................Initialize Variables
+    private int nItems=0;//....................................................................Initialize Variables
 
     // true if queue is empty
     public boolean isEmpty()
@@ -23,57 +29,51 @@ public class MyDeque {
 
     }
     public MyDeque(int s)
-    {//.......................................................................................constructor
+    {//.......................................................................................Constructor
 
-        maxSize = s;//........................................................................array is 1 cell larger
-        dequeArray = new Item[maxSize];//.....................................................than requested
+        maxSize = s;//........................................................................Array is 1 cell larger
+        dequeArray = new Item[maxSize];//.....................................................Than requested
         front = 0;
         rear = -1;
         nItems=0;
     }
-    public void insertFront(Item f)//.............................put item at front of queue
+    public void insertFront(Item f)//..........................................................Put item at front of queue
     {
-        if (isFull())
-            System.out.println("It is full");
 
-        if(front == maxSize)//...................................Front of queue
-            front = 0;//...........................................set front
-        front++;
-        dequeArray[--front] = f;//...............................increment front and insert
+        if(front == 0)//.................................................................Front of queue
+        {
+            front = maxSize;//.......................................................................Set front
+        }
+       // front++;//if omitted Causes Try Catch TO think item price is a string
+        dequeArray[--front] = f;//.............................................................Increment front and insert
         nItems++;
 
     }
 
-    public void insertRear(Item r)//...............................put item at rear of queue
+    public void insertRear(Item r)//............................................................Put item at rear of queue
     {
-        if (isFull())
-            System.out.println("Nothing Here It's Empty");
 
-        if(rear == maxSize-1)//...................................rear is set to rear of queue!
-            rear = -1;//..........................................initialize the rear
-        dequeArray[++rear] = r;//..............................increment and insert
+        if(rear == maxSize-1)//.................................................................Rear is set to rear of queue!
+            rear = -1;//........................................................................Initialize the rear
+        dequeArray[++rear] = r;//...............................................................Increment and insert
         nItems++;
     }
 
-    public Item removeFront() //................................................................take item from front of queue
+    public Item removeFront() //................................................................Take item from front of queue
     {
-        if (isEmpty())
-            System.out.println("Nothing Here It's Empty");
 
         Item temp=dequeArray[front++];//........................................................Get Value and increment
-        if(front == maxSize)//..................................................................deal w/Wrap Around
-            front=0;//..........................................................................setting the Front
-        nItems--;//.........................................................................One Less Item
+        if(front == maxSize)//..................................................................Deal w/Wrap Around
+            front=0;//..........................................................................Setting the Front
+        nItems--;//.............................................................................One Less Item
         return temp;
 
     }
     //take item from rear of queue
     public Item removeRear()
     {
-        if (isEmpty())
-            System.out.println("Nothing Here It's Empty");
 
-        Item temp=dequeArray[rear++];//.........................................................take item from rear of queue
+        Item temp=dequeArray[rear--];//.........................................................Take item from rear of queue
         if(rear==maxSize)//.....................................................................Deal with wrap around
             rear=-1;
         nItems--;
@@ -83,21 +83,25 @@ public class MyDeque {
     //peek at Front
     public Item peekFront()
     {
-        if(front== maxSize)//...................................................................Deal with wrap around
-            front=0;
+       // if(front== maxSize)//...................................................................Deal with wrap around
+         //   front=0;
         return dequeArray[front];
 
     }
     //peek at Rear
     public Item peekRear()
     {
-        if(rear== maxSize-1)//..................................................................Deal with wrap around
-            rear=-1;
+       // if(rear== maxSize-1)//..................................................................Deal with wrap around
+         //   rear=-1;
         return dequeArray[rear];
     }
 
     //Displays the data stored in the deque
     //in the sequence from ‘front’ to ‘rear’
+    //it compares front to rear then
+    //then depending if front is greater or
+    // rear is greater it returns which ever if
+    // statement proves correct
     @Override
     public String toString()
     {
@@ -124,5 +128,5 @@ public class MyDeque {
 
         return frontRear;
     }
-}
+}//End Deque Class
 
