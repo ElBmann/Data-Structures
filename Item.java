9f93cff56a629fe5739ef4254c283eb9560e41cb
ElBmann <brianrecuero@gmail.com>
2016-04-11 21:58:06 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,20 +1,27 @@
+/**
+ * Brian Recuero
+ * The item Class written in java
+ * Consists of a DEFAULT VALUE Getters and Setters
+ * for the toString method in the main class.
+ */
+
 package dataStructures;
 public class Item
 {
     String sku;
     double itemPrice;
-    public Item()
+    public Item()//..........................................Default Constructor
     {
         sku = "10.00";
         itemPrice = 100.00;
     }
-    Item(String newSku, double newItemPrice)
+    Item(String newSku, double newItemPrice)//...............Giving Item, Item Types
     {
         sku = newSku;
         itemPrice = newItemPrice;
     }
 
-    public String getSku()
+    public String getSku()//.................................Get Method
     {
         return sku;
     }
@@ -23,19 +30,19 @@ public class Item
         sku = newSku;
     }
 
-    public double getItemPrice()
+    public double getItemPrice()//............................Get Method
     {
         return itemPrice;
     }
 
-    public void setItemPrice(double newItemPrice)
+    public void setItemPrice(double newItemPrice)//...........Set method
     {
         itemPrice = newItemPrice;
     }
 @Override
-    public String toString()
+    public String toString()//.................................To String
 {
-return"Sku Product Number: "+ sku+"Item Price: "+itemPrice;
+return"\n Sku Product Number: "+ sku+"\n Item Price: "+itemPrice;
 }
 
-}
+}//End App Class
