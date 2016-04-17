65bdf830fe20990b4e81eff23eba2a315d2b0b70
ElBmann <brianrecuero@gmail.com>
2016-04-17 17:31:19 -0400

    Update RecordList.java

--- RecordList.java
+++ RecordList.java
@@ -1,62 +1,151 @@
 package HW3;
 /**
  * Created by Brecuero on 3/31/2016.
+ * Data Structures Doubly Link List
+ * Is where we Create all out methods
+ * the behind the scenes for the app class
  */
 
- class RecordList
-{
-    private Link first, last;//......................sentinel first and last
+public class RecordList {
+    private Link first, last;//......................Sentinel first and last
 
-    public  RecordList()
-    {
-        first= null;
+    public RecordList() {
+        first = null;
         last = null;
     }
-public boolean isEmpty()//....................................function checks if function is empty
- {
-    return first ==null;
-}
-public void insertFirst(int value)//..insert first Link
-{
-   Link newLink = new Link(value);//.......create a new Link
-    if(isEmpty()) {last = newLink;}//......new link to last
-        else {first.prev= newLink;}//......old first now is the previous
-        newLink.next=first;//.....The previous element is now stored in next
-        first = newLink;//........the first is assigned to this newest element
+
+    public boolean isEmpty()//....................................Function checks if function is empty
+    {
+        return first == null;
     }
-    public void insertLast(int value)
+
+    public void insertFirst(Link newLink)//....Insert first Link
     {
-       Link newLink = new Link(value);
-        if(isEmpty()) {first = newLink;}
-        else{last.next=newLink; newLink.prev = last;}
-        last = newLink;
-}
-    public Link deleteFirst()
-    {
+        newLink.next = getFirst();//..................The previous element is now stored in next
+        first = newLink;//.....................The first is assigned to this newest element
+
+    }
+
+    public void insertLast(Link newLink) {
+
+        newLink.prev = getLast();//..................The
+        last = newLink;//.......................
+
+    }
+
+    public Link deleteFirst() {
         Link temp = first;//........
-        if (first.next == null) {last = null;}
-        else {first.next.prev = null;}
+        if (first.next == null) {
+            last = null;
+        } else {
+            first.next.prev = null;
+        }
         first = first.next;
         return temp;
     }
 
-    public Link deleteLast()
-    {
-      Link temp = last;
-        if(first.next ==null){first = null;}
-        else{last.prev.next=null;}
-        last=last.prev;
+    public Link deleteLast() {
+        Link temp = last;
+        if (first.next == null) {
+            first = null;
+        } else {
+            last.prev.next = null;
+        }
+        last = last.prev;
         return temp;
     }
 
-  /*
-    public boolean insertAfter()
-    {
 
+    public void insertAfter(String id, Link newLink)//.....Key,Element new Node
+    {
+        Link current = first;//..................Starts at first link list
+        while (current != null) {
+            if (current.data.getId().compareTo(id) == 0)//....Make sure that id in data is id
+            {
+                newLink.next = current.next;
+                newLink.prev = current;
+                current.next.prev = newLink;
+                current.next = newLink;
+            } else current = current.next;
+        }
     }
-    @Override
-    public String toString()
-    {
 
-    }*/
+
+    /*   public void insertBefore(String id, Link newLink)
+   {
+       Link current = last;//.............Starts at last Link
+       while(current!= null)
+       {
+           if (current.data.getId().compareTo(id) == 0)
+           {
+               newLink.next = current.next;
+               newLink.prev = current;
+               current.next.prev = newLink;
+               current.next = newLink;
+           } else current = current.prev;
+       }
+
+   }*/
+    public Link deleteKey(String id) {//....Need a marker
+        Link current = first;
+        while (current.data.getId() != id) {
+            current = current.next;
+            if (current == null)
+                return null;
+        }
+        if (current == first) {
+            first = current.next;
+        } else {
+            current.prev.next = current.prev;
+        }
+        if (current == last) {
+            last = current.prev;
+        } else {
+            current.next.prev = current.prev;
+        }
+        return current;
+    }
+
+    public Link getFirst() {
+        return first;
+    }
+
+    public Link getLast() {
+        return last;
+    }
+
+    public void setLast(Link newLink) {
+        last = newLink;
+    }
+
+    public void setFirst(Link newLink) {
+        first = newLink;
+    }
+   /* public void insertionSort(Student files )
+    {
+        Link Current = getFirst().next;
+        Link Current2 = getFirst().next;
+          for (int i = 1; i < input.length; i++) {
+            for(int j = i ; j > 0 ; j--){
+                if(input[j] < input[j-1]){
+                    temp = input[j];
+                    input[j] = input[j-1];
+                    input[j-1] = temp;
+                }
+            }
+        }
+        }*/
+
+
+    @Override
+    public String toString() {
+        Link Temp = getFirst();
+        String studentInfo = "";
+        while (Temp != null) {
+            studentInfo += Temp.toString();
+            Temp = Temp.next;
+
+        }
+        return studentInfo;
+    }
 }
