63ef4828309f76f2890e93230eb53ba57f8f08ea
ElBmann <brianrecuero@gmail.com>
2016-04-17 16:01:33 -0400

    Update Link.java

--- Link.java
+++ Link.java
@@ -1,17 +1,26 @@
 package HW3;
 /**
  * Created by Brecuero on 4/1/2016.
- *
-        */
-class Link
+ *Data Structures Doubly Link List
+ * Link Is used to initalize prev,and next and the the student Data
+ */
+
+public class Link
 {
     public Link prev, next;//..........sentinel before & after
-    public int data;//.................initialize Data
+    public Student data;//.................initialize Data student is data now
 
-    public Link(int d)
+
+    public Link(Student files)
     {
         next = null;//..................set next to null
         prev = null;//..................set prev to null
-        data =0;
+        data = files;
+
+    }
+    @Override
+    public String toString()
+    {
+       return data+"";
     }
 }
